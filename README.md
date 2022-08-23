307729a1c9621296b8a95dca01b4f15bf998e5f3
Digitalguyco <youngkhito@gmail.com>
2022-08-23 10:45:19 -0700

    First Push

---
+++ README.md
@@ -0,0 +1,12 @@
+## Built Starkbucks index page clone with Tailwind CSS
+
+
+
+#### Screenshots
+
+![X](./Sc1.png)
+
+
+![XX](./Sc2.png)
+
+![XXX](./Sc3.png)
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        sm: ["17px"],
+  
+      },
+      colors: {
+        starGreen: "#D4E9E2",
+        darkGreen: "#1E3932",
+        lightText: "rgba(0,0,0, .58)",
+        titleText: "rgba(0,0,0, .87)",
+      },
+    },
+  },
+  plugins: [],
+};
